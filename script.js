c775a8bbf52a9fb345046ff9b22cead7d4382167
Quan Ming Vang <mingvang2003@gmail.com>
2025-01-04 06:50:42 +0000

    Function getComputerChoice added to randomly return rock, paper or scissors.

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    randomInt = Math.floor(Math.random() * 3);
+    if (randomInt === 0) {
+        return "rock";
+    }
+    else if (randomInt === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
