cde1076ec33a8d7e5ae3dde6332efa98b1aab32e
Quan Ming Vang <mingvang2003@gmail.com>
2025-01-04 09:47:52 +0000

    Logic for human choice added.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    randomInt = Math.floor(Math.random() * 3);
+    let randomInt = Math.floor(Math.random() * 3);
     if (randomInt === 0) {
         return "rock";
     }
@@ -11,4 +11,18 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper, scissors?");
+    if (humanChoice.toLowerCase() === "rock") {
+        return "rock";
+    }
+    else if (humanChoice.toLowerCase() === "paper") {
+        return "paper";
+    }
+    else if (humanChoice.toLowerCase() === "scissors") {
+        return "scissors";
+    }
+    else {
+        getHumanChoice();
+    }
+}
